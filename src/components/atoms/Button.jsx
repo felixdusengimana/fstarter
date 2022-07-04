fdee851b3d6f2807f79bb2702946866d3f096e49
phelixdusengimana <phelixdusengimana@gmail.com>
2022-07-04 21:55:02 +0200

    feat: table and button component done

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function Button({theme}) {
-  const btnTheme = theme==='primary'?'btn-primary'
-                    :theme==='success'?'btn-success border-success'
-                    :theme==='warning'?'btn-warning'
-                    :theme==='danger'?'btn-danger':'btn-default';
+
   return (
-    <button className={`btn ${btnTheme}`}>Button</button>
+    <button style={{
+        backgroundColor: theme.background,
+        color: theme.color,
+        borderColor: theme.border,
+    }} className={`btn btn-${theme.name}`}>Button</button>
   )
 }
