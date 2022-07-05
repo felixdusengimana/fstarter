02050b83ad4510673767ffb5228042ca1010e5cf
phelixdusengimana <phelixdusengimana@gmail.com>
2022-07-05 21:59:25 +0200

    feat: docker files

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Button({theme}) {
+export default function Button({theme, children, onClick, disabled}) {
   const btnTheme = theme==='primary'?'btn-primary'
                     :theme==='success'?'btn-success border-success'
                     :theme==='warning'?'btn-warning'
                     :theme==='danger'?'btn-danger':'btn-default';
   return (
-    <button className={`btn ${btnTheme}`}>Button</button>
+    <button className={`btn ${btnTheme}`} disabled={disabled} onClick={onClick}>{children}</button>
   )
 }
